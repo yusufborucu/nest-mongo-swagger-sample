80087bb5382809eee7ce31e775f0d94856699b94
Yusuf Borucu <yusufborucu@gmail.com>
2022-07-05 15:53:16 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Nest Mongo Swagger Sample
+
+NestJS üzerinde MongoDB ve Swagger kullanımına yönelik örnek proje.
+
+Detaylı anlatım için: https://kodumunblogu.net/detail/nestjs-uzerinde-mongodb-ve-swagger-kullanimi
---
+++ src/users/users.controller.ts
@@ -0,0 +1,41 @@
+import { 
+  Controller, 
+  Post, 
+  Get, 
+  Body,
+  Patch,
+  Param,
+  Delete
+} from '@nestjs/common';
+import { User } from './users.model';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  create(@Body() createUserDto: User) {
+    return this.usersService.create(createUserDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.usersService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: User) {
+    return this.usersService.update(id, updateUserDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.usersService.remove(id);
+  }
+}
